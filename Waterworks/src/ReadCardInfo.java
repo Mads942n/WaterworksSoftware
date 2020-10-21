ecbd02b70927d1124f0feba9adb808b23f56ccc7
Raheela29 <rahe006@easv365.dk>
2020-10-21 10:55:58 +0200

    Read card info start.

--- Waterworks/src/ReadCardInfo.java
+++ Waterworks/src/ReadCardInfo.java
@@ -1,11 +1,18 @@
-
-
-
+import java.util.Scanner;
 
 public class ReadCardInfo {
 
 
+        public static String input() {
+            String customerCard;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Type customer card number:");
+            //Scanner in = new Scanner(System.in);
+            // Scanner CN = new Scanner(System.in);
+            String cardNumber = in.nextLine();
+            customerCard = in.nextLine();
+            return customerCard;
+        }
+    }
 
 
-
-}
--- Waterworks/src/WaterworksMain.java
+++ Waterworks/src/WaterworksMain.java
@@ -1,8 +1,10 @@
-
+import java.util.Scanner;
 
 public class WaterworksMain {
 
     public static void main(String[] args) {
 
-    }
-}
+        ReadCardInfo.input();
+
+
+    }}
